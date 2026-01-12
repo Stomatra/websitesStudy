c4ee5b41cdd26f6ec8b82e3edf3325e3b63935fc
Stoma <3223572901@qq.com>
2026-01-12 11:31:21 +0800

    添加项目文件。

---
+++ 2025-10-8/2025-10-8-e2/script.js
@@ -0,0 +1,7 @@
+﻿const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    btn.textContent = "你点击了这个按钮！！ ❤️";
+    setTimeout(() => {
+        btn.textContent = "点击这里";
+    }, 1000);
+});
